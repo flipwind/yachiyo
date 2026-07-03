e3421022227013a41c9b914924079a38c0ada46e
flipwind <flipwind.t@gmail.com>
2026-07-04 00:47:22 +0800

    perf(runtime): simplify channel bind

--- yachiyo-runtime/server.go
+++ yachiyo-runtime/server.go
@@ -18,59 +18,12 @@ var ylog = logger.New("Yachiyo.Server.Main")
 func main() {
 	ylog.Info("Initializing Yachiyo server...")
 
-	var core_copy core.Core
 	core := core.New()
 
 	ylog.Success("Successfully initialize Yachiyo server.")
 
-	gatewayChanOnebot := gateway.NewGatewayChannel()
-	onebot.Service(gatewayChanOnebot)
-
-	gatewayChanClient := gateway.NewGatewayChannel()
-	client.Service(gatewayChanClient)
-
-	// Reading channel
-	go func() {
-		for msg := range gatewayChanClient.ToServer {
-			ylog.Debug("Processing [%s]", msg.Content)
-			core_copy = *core
-			result := core.Process(&msg)
-
-			fmt.Printf("Yachiyo > %s\n", result)
-
-			fmt.Printf("\nDEBUG\nFORMAL:")
-			fmt.Println(core_copy.Emotion.String())
-			fmt.Println(core_copy.State.Prompt())
-			fmt.Printf("\nLATER:\n")
-			fmt.Println(core.Emotion.String())
-			fmt.Println(core.State.Prompt())
-
-			msg.Content = result
-
-			gatewayChanClient.ToClient <- msg
-		}
-	}()
-
-	go func() {
-		for msg := range gatewayChanOnebot.ToServer {
-			ylog.Debug("Processing [%s]", msg.Content)
-			core_copy = *core
-			result := core.Process(&msg)
-
-			fmt.Printf("Yachiyo > %s\n", result)
-
-			fmt.Printf("\nDEBUG\nFORMAL:")
-			fmt.Println(core_copy.Emotion.String())
-			fmt.Println(core_copy.State.Prompt())
-			fmt.Printf("\nLATER:\n")
-			fmt.Println(core.Emotion.String())
-			fmt.Println(core.State.Prompt())
-
-			msg.Content = result
-
-			gatewayChanOnebot.ToClient <- msg
-		}
-	}()
+	go serviveChannel(core, onebot.Service)
+	go serviveChannel(core, client.Service)
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
@@ -78,4 +31,28 @@ func main() {
 	<-ctx.Done()
 
 	ylog.Info("Shutting down...")
+}
+
+func serviveChannel(c *core.Core, gw func(ch *gateway.GatewayChannel)) {
+	gatewayChannel := gateway.NewGatewayChannel()
+	gw(gatewayChannel)
+
+	for msg := range gatewayChannel.ToServer {
+		ylog.Debug("Processing [%s]", msg.Content)
+		core_copy := *c
+		result := c.Process(&msg)
+
+		fmt.Printf("Yachiyo > %s\n", result)
+
+		fmt.Printf("\nDEBUG\nFORMAL:")
+		fmt.Println(core_copy.Emotion.String())
+		fmt.Println(core_copy.State.Prompt())
+		fmt.Printf("\nLATER:\n")
+		fmt.Println(c.Emotion.String())
+		fmt.Println(c.State.Prompt())
+
+		msg.Content = result
+
+		gatewayChannel.ToClient <- msg
+	}
 }
