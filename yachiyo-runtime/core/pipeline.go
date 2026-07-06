a65d4bd05a4e36d0ad00b8d37d691331de6a763e
flipwind <flipwind.t@gmail.com>
2026-07-07 02:48:25 +0800

    refactor(runtime): tidy pipeline and core

--- yachiyo-runtime/core/pipeline.go
+++ yachiyo-runtime/core/pipeline.go
@@ -1,9 +1,6 @@
 package core
 
 import (
-	"fmt"
-	"yachiyo/yachiyo-runtime/history"
-	"yachiyo/yachiyo-runtime/prompt"
 	"yachiyo/yachiyo-runtime/trigger"
 )
 
@@ -31,73 +28,8 @@ func (p *Pipeline) Register(scheme string, outputChan *chan trigger.Message) {
 // Listen and process.
 func (p *Pipeline) Listen() {
 	for e := range p.Raw {
-		// TODO: Let core process it.
-		histories := prompt.PromptBuilder(&prompt.Context{
-			History: p.core.History,
-			Emotion: p.core.Emotion,
-			State:   p.core.State,
-			Note:    p.core.Note,
-		}, e)
-
-		var result, answer string
-		var err error
-
-		// <debug>
-		var core_copy Core
-		core_copy = *p.core
-		// </debug>
-
-		for i := range 3 {
-			if i == 1 {
-				p.core.JSONConstraint = true
-			}
-
-			if p.core.JSONConstraint {
-				histories = append(histories, history.History{
-					Role:    "user",
-					Content: "<PROCESS HINT> YOUR LAST REPLY IS NOT A VALID JSON, REGENERATE IT. YOU MUST FOLLOW THE OUTPUT ROLE.",
-				})
-			}
-
-			result, err = p.core.LLM.Gen(histories)
-
-			if err != nil {
-				ylog.Error("LLM Generating error: %v", err)
-				continue
-			}
-
-			answer, err = p.core.OutputProcess(result)
-
-			if err != nil {
-				ylog.Error("%d request failed. Retrying...", i+1)
-				continue
-			}
-
-			p.core.History.Remember(history.History{
-				Role:    "assistant",
-				Content: answer,
-			})
-
-			break
-		}
-
-		// <debug>
-		fmt.Printf("Yachiyo > %s\n", answer)
-
-		fmt.Printf("\nDEBUG\nFORMAL:")
-		fmt.Println(core_copy.Emotion.String())
-		fmt.Println(core_copy.State.Prompt())
-		fmt.Printf("\nLATER:\n")
-		fmt.Println(p.core.Emotion.String())
-		fmt.Println(p.core.State.Prompt())
-		fmt.Println(p.core.Determination)
-
-		fmt.Println("Session Context: " + p.core.Note)
-		// </debug>
-
-		// TODO: e => PipelineResult
-		e.Content = answer
-		p.Distribution <- e
+		dispatch := p.core.Process(e)
+		p.Distribution <- dispatch
 	}
 }
 
--- yachiyo-runtime/core/process.go
+++ yachiyo-runtime/core/process.go
@@ -2,8 +2,87 @@ package core
 
 import (
 	"encoding/json"
+	"fmt"
+	"time"
+	"yachiyo/yachiyo-runtime/history"
+	"yachiyo/yachiyo-runtime/prompt"
 	"yachiyo/yachiyo-runtime/state"
+	"yachiyo/yachiyo-runtime/trigger"
 )
+
+func (c *Core) Process(e *trigger.Message) *trigger.Message {
+	histories := prompt.PromptBuilder(&prompt.Context{
+		History: c.History,
+		Emotion: c.Emotion,
+		State:   c.State,
+		Note:    c.Note,
+	}, e)
+
+	var result, answer string
+	var err error
+
+	// <debug>
+	var core_copy Core
+	core_copy = *c
+	// </debug>
+
+	for i := range 3 {
+		if i == 1 {
+			c.JSONConstraint = true
+		}
+
+		if c.JSONConstraint {
+			histories = append(histories, history.History{
+				Role:    "user",
+				Content: "<PROCESS HINT> YOUR LAST REPLY IS NOT A VALID JSON, REGENERATE IT. YOU MUST FOLLOW THE OUTPUT ROLE.",
+			})
+		}
+
+		result, err = c.LLM.Gen(histories)
+
+		if err != nil {
+			ylog.Error("LLM Generating error: %v", err)
+			continue
+		}
+
+		answer, err = c.OutputProcess(result)
+
+		if err != nil {
+			ylog.Error("%d request failed. Retrying...", i+1)
+			continue
+		}
+
+		c.History.Remember(history.History{
+			Role:    "assistant",
+			Content: answer,
+		})
+
+		break
+	}
+
+	// <debug>
+	fmt.Printf("Yachiyo > %s\n", answer)
+
+	fmt.Printf("\nDEBUG\nFORMAL:")
+	fmt.Println(core_copy.Emotion.String())
+	fmt.Println(core_copy.State.Prompt())
+	fmt.Printf("\nLATER:\n")
+	fmt.Println(c.Emotion.String())
+	fmt.Println(c.State.Prompt())
+	fmt.Println(c.Determination)
+
+	fmt.Println("Session Context: " + c.Note)
+	// </debug>
+
+	return &trigger.Message{
+		Type: e.Type,
+		Author: e.Author,
+		Platform: e.Platform,
+		Content: answer,
+		Time: time.Now().Unix(),
+		Address: e.Address,
+	}
+}
 
 type LLMOutput struct {
 	Answer string `json:"answer"`
@@ -14,11 +93,11 @@ type LLMOutput struct {
 		} `json:"emotion"`
 		State struct {
 			SocialDesire int64 `json:"SocialDesire"`
-			Interest      int64 `json:"Interest"`
+			Interest     int64 `json:"Interest"`
 		} `json:"state_delta"`
 	} `json:"change"`
 	Determination state.Determination `json:"determination"`
-	Note string `json:"note"`
+	Note          string              `json:"note"`
 }
 
 func (c *Core) OutputProcess(schema string) (string, error) {
